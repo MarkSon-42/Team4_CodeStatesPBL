07e4c133e5a02d0e1a8db807242bf063ecd00087
minwoo <mark42son@gmail.com>
2023-07-05 13:54:42 +0900

    create : models.py added class AdInfo(),..

---
+++ AD_Server/AD_Server/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class AdInfo(models.Model):
+    media_type = models.ForeignKey('MediaTypeInfo', models.DO_NOTHING, blank=True, null=True)
+    name = models.CharField(max_length=45, blank=True, null=True)
+    start_date = models.DateTimeField(blank=True, null=True)
+    end_date = models.DateTimeField(blank=True, null=True)
+    mod_date = models.DateTimeField(blank=True, null=True)
+    definition = models.CharField(max_length=500, blank=True, null=True)
+    cost = models.IntegerField(blank=True, null=True)
+    advertiser = models.CharField(max_length=45, blank=True, null=True)
+    click_cnt = models.IntegerField(blank=True, null=True)
+    url = models.CharField(max_length=255, blank=True, null=True)
+    content_path = models.CharField(max_length=255, blank=True, null=True)
+    type = models.CharField(max_length=45, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'AD_Info'
