67c1ae691455cdd16f68a0209219ad3421208cc5
eojin <dpinlx@kookmin.ac.kr>
2023-07-06 15:38:22 +0900

    feature : build main page board ,html, ....

--- Global_AD_Server/Global_AD_Server/urls.py
+++ Global_AD_Server/Global_AD_Server/urls.py
@@ -20,5 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('board/', include('board.urls')),
-    path('adtest/', include('adtest.urls')),
 ]
--- Global_AD_Server/adtest/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'adtest'
-urlpatterns = [
-    path('index/', views.index, name="index"),
-    # path('index/webview/', views.webview, name="webview"),
-]
