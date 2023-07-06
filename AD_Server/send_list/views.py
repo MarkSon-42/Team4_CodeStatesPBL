39bafeaf2f4fba82bfb0a6436af746f0e3415b34
minwoo <mark42son@gmail.com>
2023-07-06 11:27:02 +0900

    added new function index() for blacklist ip processing in views.py

--- AD_Server/send_list/views.py
+++ AD_Server/send_list/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
-from django.core import serializers
-# Create your views here.
-from django.http import JsonResponse
-from django.views import View
-from django.views.generic import ListView
-from .models import AdInfo
-class AdListView(View):
-    def get(self, request):
-        ads = AdInfo.objects.all() #이 안에 AdInfo테이블안에 모든 튜플을 가져다 놓음
-        post_list = serializers.serialize('json', ads) # 그리고 그걸 list 형태로 바꿈 
-        responseData = {'data' : post_list}
-        return JsonResponse(responseData)
+from django.http import HttpResponseForbidden
+from .models import Blacklist
+
+def index(request):
+    ip_address = request.META.get('REMOTE_ADDR')
+    blocked_ips = Blacklist.objects.values_list('ipaddress', flat=True)
+    
+    if ip_address in blocked_ips:
+        return HttpResponseForbidden("접근이 차단되었습니다.")
+    
+    # 접근이 허용된 경우, 원하는 동작을 수행하는 코드 작성
+    # ...
+    
+    return render(request, 'index.html')
