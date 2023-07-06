787947b92b615f6cc65d401b5fcb822c7e4218d8
minjun7410 <minjun7410@gmail.com>
2023-07-06 10:15:42 +0900

    fix conflict (main <- minjun-brunch)

--- AD_Server/AD_Server/urls.py
+++ AD_Server/AD_Server/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [=
+urlpatterns = [
     path('admin/', admin.site.urls),
     path('test_list/', include('test_list.urls'))
 ]
