7ede7b5d67c4c950254ad429334540ced5e632d1
minwoo <mark42son@gmail.com>
2023-07-06 18:02:00 +0900

    feature : define new function for reponse

--- AD_Server/send_list/apps.py
+++ AD_Server/send_list/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 
 
+# ------ Django app설정파일인 SendListConfig 클래스를 정의
 class SendListConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
+    # 애플리케이션에서 사용할 기본 자동 필드 타입을 지정
     name = 'send_list'
+    # name : 앱의 이름을 지정
